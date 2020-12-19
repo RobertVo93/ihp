553e2cf455fc03132541be04c12236efba727fc4
vo.hoang <vo.hoang@fullertonhealth.com>
2020-12-19 22:14:02 +0700

    Updates

---
+++ precache-manifest.b3beb5913b0d2433f49609c120822812.js
@@ -0,0 +1,610 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edd2454840e886f05506391b3d2bb75",
+    "url": "/ihp/index.html"
+  },
+  {
+    "revision": "38aeae2c23cdd08877ee",
+    "url": "/ihp/static/css/2.927ffd3c.chunk.css"
+  },
+  {
+    "revision": "c7e285d7a9dbb0939503",
+    "url": "/ihp/static/css/main.b9cd75f1.chunk.css"
+  },
+  {
+    "revision": "38aeae2c23cdd08877ee",
+    "url": "/ihp/static/js/2.07ec437f.chunk.js"
+  },
+  {
+    "revision": "2379e70f1e01d8e1f4894186dd647868",
+    "url": "/ihp/static/js/2.07ec437f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e285d7a9dbb0939503",
+    "url": "/ihp/static/js/main.d90e6f72.chunk.js"
+  },
+  {
+    "revision": "99cf17615925c904e48d",
+    "url": "/ihp/static/js/runtime-main.a6e577b5.js"
+  },
+  {
+    "revision": "f1a69fda5b6be863f0e15c754819c81b",
+    "url": "/ihp/static/media/6464Img.f1a69fda.svg"
+  },
+  {
+    "revision": "38cfdcb996876ed91ec9715b421272e3",
+    "url": "/ihp/static/media/Assign.38cfdcb9.svg"
+  },
+  {
+    "revision": "c1fb8fb664bef8d406dfeb6027179296",
+    "url": "/ihp/static/media/Facebook.c1fb8fb6.svg"
+  },
+  {
+    "revision": "adceb3fc0efd4dfead31d006c9f06797",
+    "url": "/ihp/static/media/LinkedIN.adceb3fc.svg"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/ihp/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/ihp/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/ihp/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/ihp/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "61590e5394cda66f0dedbec9c483bc7f",
+    "url": "/ihp/static/media/Messages.61590e53.svg"
+  },
+  {
+    "revision": "44ea8275e878ed72a9c8a393f9de12d2",
+    "url": "/ihp/static/media/Muli-Bold.44ea8275.ttf"
+  },
+  {
+    "revision": "d80af0828fe7e5236cb28e3bbcdf9e87",
+    "url": "/ihp/static/media/Muli-Light.d80af082.ttf"
+  },
+  {
+    "revision": "e063a333cc9d58a3ad85d5cd86bf92d5",
+    "url": "/ihp/static/media/Muli-Regular.e063a333.ttf"
+  },
+  {
+    "revision": "3217b2cde8e4451eb8a8572d39798598",
+    "url": "/ihp/static/media/Muli-SemiBold.3217b2cd.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/ihp/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "1a055e96c2be5b39bd68605558c06b6d",
+    "url": "/ihp/static/media/Twitter.1a055e96.svg"
+  },
+  {
+    "revision": "d41845fd6ab720f47a9ece1f9380896b",
+    "url": "/ihp/static/media/active-grid-icon.d41845fd.svg"
+  },
+  {
+    "revision": "fe4a2c4d66a134f774ca88e2e446be8c",
+    "url": "/ihp/static/media/active-list-icon.fe4a2c4d.svg"
+  },
+  {
+    "revision": "ca8c9251a0f9a99e6fa99387332549e9",
+    "url": "/ihp/static/media/avatar.ca8c9251.svg"
+  },
+  {
+    "revision": "dd5b8dd176be0a1d2bc75e7097587e93",
+    "url": "/ihp/static/media/avg-claim-visit.dd5b8dd1.svg"
+  },
+  {
+    "revision": "632233f9796a38ae6c9dfa8c97578e2d",
+    "url": "/ihp/static/media/avg-panel-claim-value-bg.632233f9.svg"
+  },
+  {
+    "revision": "70cc84d35af13cdfd83cb6c68b8908cc",
+    "url": "/ihp/static/media/avg-panel-claim-value-icon.70cc84d3.svg"
+  },
+  {
+    "revision": "3e7e1faf3a2dc039d4c3ee37aa5551db",
+    "url": "/ihp/static/media/back-button-icon.3e7e1faf.svg"
+  },
+  {
+    "revision": "f57ec15d56b4d3c5ab41bea3a1d8b89f",
+    "url": "/ihp/static/media/back-button.f57ec15d.svg"
+  },
+  {
+    "revision": "4f5837907fd610f53b027fee6367458a",
+    "url": "/ihp/static/media/backgroundImage.4f583790.png"
+  },
+  {
+    "revision": "b6940fcd21654398a8334899f1b9bcb7",
+    "url": "/ihp/static/media/bg-shape.b6940fcd.png"
+  },
+  {
+    "revision": "8881a2352a6ab06b443d23023acd7470",
+    "url": "/ihp/static/media/brown-shoe.8881a235.jpeg"
+  },
+  {
+    "revision": "ba5b2bc1f2b0f76f0c8779a85ccf9187",
+    "url": "/ihp/static/media/busy.ba5b2bc1.svg"
+  },
+  {
+    "revision": "7487bd275b8fd76ebe7303981416a1ca",
+    "url": "/ihp/static/media/calendar-icon.7487bd27.svg"
+  },
+  {
+    "revision": "efc229e3ba6ad0edd63239f53ccaeb66",
+    "url": "/ihp/static/media/cancel-newfeature-button-icon.efc229e3.svg"
+  },
+  {
+    "revision": "4e4d59b7a0903071d743973f8e3b3aab",
+    "url": "/ihp/static/media/china.4e4d59b7.svg"
+  },
+  {
+    "revision": "02835f1f7fe4d675c635544bf4f8872e",
+    "url": "/ihp/static/media/cover1.02835f1f.jpeg"
+  },
+  {
+    "revision": "12c6f5d7522676d3af7ee440f4899b36",
+    "url": "/ihp/static/media/cover10.12c6f5d7.jpeg"
+  },
+  {
+    "revision": "5a8f7cd1762fcba4a19ee8a1fe03c83d",
+    "url": "/ihp/static/media/cover2.5a8f7cd1.jpeg"
+  },
+  {
+    "revision": "4875e7ae129cdced941f1b223944c5d4",
+    "url": "/ihp/static/media/cover3.4875e7ae.jpeg"
+  },
+  {
+    "revision": "04254933160582266d58d45c85316925",
+    "url": "/ihp/static/media/cover4.04254933.jpg"
+  },
+  {
+    "revision": "e2886eb1717d3ff7a9f8ef58223c537b",
+    "url": "/ihp/static/media/cover5.e2886eb1.jpeg"
+  },
+  {
+    "revision": "970781b349f2eb78d880f6514a877809",
+    "url": "/ihp/static/media/cover6.970781b3.jpeg"
+  },
+  {
+    "revision": "bcf5c8af2c00df8e0a63d23e3f2016d6",
+    "url": "/ihp/static/media/cover7.bcf5c8af.jpeg"
+  },
+  {
+    "revision": "c341a5bb0b3281b77b25f1c041693574",
+    "url": "/ihp/static/media/cover8.c341a5bb.jpeg"
+  },
+  {
+    "revision": "da71cb5b3690f53ad07308a98d8ebc0b",
+    "url": "/ihp/static/media/cover9.da71cb5b.jpeg"
+  },
+  {
+    "revision": "d872e5001e74d0a28a5e8eefb7510fe7",
+    "url": "/ihp/static/media/demo1.d872e500.PNG"
+  },
+  {
+    "revision": "1fb910893fb3358f44f80277d7d94ef1",
+    "url": "/ihp/static/media/demo2.1fb91089.png"
+  },
+  {
+    "revision": "69df642ebd5c470be33437c6581039b2",
+    "url": "/ihp/static/media/demo3.69df642e.png"
+  },
+  {
+    "revision": "976050265b53f3d029efaefa23d9a3e0",
+    "url": "/ihp/static/media/demo4.97605026.png"
+  },
+  {
+    "revision": "45a358d6342f77ff7ebeb2c4a999c3bc",
+    "url": "/ihp/static/media/demo5.45a358d6.png"
+  },
+  {
+    "revision": "b6ef7b5c938f906bfcf5835f58e98209",
+    "url": "/ihp/static/media/email-icon.b6ef7b5c.svg"
+  },
+  {
+    "revision": "abf69d2d0d12cb47aefa8da250ac4101",
+    "url": "/ihp/static/media/errorback.abf69d2d.jpg"
+  },
+  {
+    "revision": "a0772dc57eef1f09ea1ad5d146bd3fdf",
+    "url": "/ihp/static/media/feed1.a0772dc5.jpg"
+  },
+  {
+    "revision": "2a896b479133afc4a15dc4bdfe65cecc",
+    "url": "/ihp/static/media/feed2.2a896b47.jpg"
+  },
+  {
+    "revision": "2461d86be9300ee70c7bad8cedf32eda",
+    "url": "/ihp/static/media/file1.2461d86b.jpg"
+  },
+  {
+    "revision": "e7bf119c3f884799c3f367eb0bc06040",
+    "url": "/ihp/static/media/file10.e7bf119c.jpg"
+  },
+  {
+    "revision": "69632becd89b66eaa0d33d57154ef48b",
+    "url": "/ihp/static/media/file11.69632bec.jpg"
+  },
+  {
+    "revision": "e5180fa49a6de539701700a315324021",
+    "url": "/ihp/static/media/file12.e5180fa4.jpg"
+  },
+  {
+    "revision": "d6bcaa82f290d8c2ceee697e8232ca4b",
+    "url": "/ihp/static/media/file13.d6bcaa82.jpg"
+  },
+  {
+    "revision": "95b145bfb79c3e2e1cba576780b29631",
+    "url": "/ihp/static/media/file14.95b145bf.jpg"
+  },
+  {
+    "revision": "6d4ac6acd31d538ac90d763a1f179aea",
+    "url": "/ihp/static/media/file15.6d4ac6ac.jpg"
+  },
+  {
+    "revision": "16b8d756f5f839a86062a1c559e3b003",
+    "url": "/ihp/static/media/file16.16b8d756.jpg"
+  },
+  {
+    "revision": "b1f508bd4ba017ef64cff7e9a033f35b",
+    "url": "/ihp/static/media/file17.b1f508bd.jpg"
+  },
+  {
+    "revision": "bd08971d15e785258ae6112bba407ba6",
+    "url": "/ihp/static/media/file18.bd08971d.jpg"
+  },
+  {
+    "revision": "72e364954a27271b431b967338ef59fa",
+    "url": "/ihp/static/media/file2.72e36495.jpg"
+  },
+  {
+    "revision": "7b87dae80f118fe1a93003e12d7be312",
+    "url": "/ihp/static/media/file3.7b87dae8.jpg"
+  },
+  {
+    "revision": "4974e0d5f6adc0d2d3af98b203a5bf13",
+    "url": "/ihp/static/media/file4.4974e0d5.jpg"
+  },
+  {
+    "revision": "23b141f9e790bea9507216159c6132ce",
+    "url": "/ihp/static/media/file5.23b141f9.jpg"
+  },
+  {
+    "revision": "eae0f69cac0251e77220e6e81ccfe50c",
+    "url": "/ihp/static/media/file6.eae0f69c.jpg"
+  },
+  {
+    "revision": "206bc2ad706e415bd3a228b82dfc981a",
+    "url": "/ihp/static/media/file7.206bc2ad.jpg"
+  },
+  {
+    "revision": "2120e31d2debb33ad94bbba5fab12ae1",
+    "url": "/ihp/static/media/file8.2120e31d.jpg"
+  },
+  {
+    "revision": "0a43949d2c003b2d4e79157d3a8b7a99",
+    "url": "/ihp/static/media/file9.0a43949d.jpg"
+  },
+  {
+    "revision": "647733aa72104037a7572b053dc1b3f6",
+    "url": "/ihp/static/media/forgot-password-completion-bg.647733aa.svg"
+  },
+  {
+    "revision": "5306f8864a54a4edb949b3a73ade6f9a",
+    "url": "/ihp/static/media/forgotpassword.5306f886.svg"
+  },
+  {
+    "revision": "bcd389debadb2cf1c0332df62b80c1b9",
+    "url": "/ihp/static/media/france.bcd389de.svg"
+  },
+  {
+    "revision": "4544cd6660b538396a35fc3b8cebd1d5",
+    "url": "/ihp/static/media/grid.4544cd66.svg"
+  },
+  {
+    "revision": "4942d38c2f722b851ae1ac30131854d5",
+    "url": "/ihp/static/media/idle.4942d38c.svg"
+  },
+  {
+    "revision": "1365f7c3e03c1112eac477ee24c38657",
+    "url": "/ihp/static/media/ihp-white-logo.1365f7c3.svg"
+  },
+  {
+    "revision": "cda72012cda44dcdd0d547c9282f489c",
+    "url": "/ihp/static/media/ihp_logo.cda72012.png"
+  },
+  {
+    "revision": "33e42b6231ee5d66c82fc4d328fd8744",
+    "url": "/ihp/static/media/ihp_logo2.33e42b62.svg"
+  },
+  {
+    "revision": "30b0dacfa66a4e23a6d585880b2c0533",
+    "url": "/ihp/static/media/inactive-grid-icon.30b0dacf.svg"
+  },
+  {
+    "revision": "be996fd5f641787c9c1df62db44290ad",
+    "url": "/ihp/static/media/inactive-list-icon.be996fd5.svg"
+  },
+  {
+    "revision": "526fc4f895e78c70b12d30fc700458cd",
+    "url": "/ihp/static/media/insights-tooltips-logo.526fc4f8.svg"
+  },
+  {
+    "revision": "65ae1a3aa45be6704e14016d75d144a2",
+    "url": "/ihp/static/media/invisible.65ae1a3a.svg"
+  },
+  {
+    "revision": "d23c795d53c26bbc8d63f45aea02e94f",
+    "url": "/ihp/static/media/key.d23c795d.svg"
+  },
+  {
+    "revision": "e331abacd866301161974caf0d446b44",
+    "url": "/ihp/static/media/lock.e331abac.svg"
+  },
+  {
+    "revision": "eb4633ccf68c00f9e11107c031d39a75",
+    "url": "/ihp/static/media/lockscreen.eb4633cc.jpg"
+  },
+  {
+    "revision": "9bc7364dc6471670ccd711614bcf5a00",
+    "url": "/ihp/static/media/loginback.9bc7364d.jpg"
+  },
+  {
+    "revision": "54bd429f69b4014c98f788ec1159c461",
+    "url": "/ihp/static/media/logout.54bd429f.svg"
+  },
+  {
+    "revision": "96eff5e24d6f1443399141abb3dec8b6",
+    "url": "/ihp/static/media/mail.96eff5e2.svg"
+  },
+  {
+    "revision": "4d97a33d0ee83e7a55d68eef0605ae52",
+    "url": "/ihp/static/media/media1.4d97a33d.jpeg"
+  },
+  {
+    "revision": "b69591e0969950a1f303802cc6bb6fa9",
+    "url": "/ihp/static/media/media5.b69591e0.jpeg"
+  },
+  {
+    "revision": "7c4dc1892ea40ab2e373853957c22515",
+    "url": "/ihp/static/media/media6.7c4dc189.jpeg"
+  },
+  {
+    "revision": "8751f76c5b913ee5dfdc719ed169ef59",
+    "url": "/ihp/static/media/media8.8751f76c.jpeg"
+  },
+  {
+    "revision": "1611d49a158597f62bcedc9fd4abbb8f",
+    "url": "/ihp/static/media/new-features-bg.1611d49a.svg"
+  },
+  {
+    "revision": "5a7850b33c93449559b9757830cb66ad",
+    "url": "/ihp/static/media/next-button-icon.5a7850b3.svg"
+  },
+  {
+    "revision": "8dcc41fca5dd3a52179a0b3f34bacc21",
+    "url": "/ihp/static/media/noti-icon.8dcc41fc.svg"
+  },
+  {
+    "revision": "fe1e3040ded8f3ab63ae250f67192b2b",
+    "url": "/ihp/static/media/online.fe1e3040.svg"
+  },
+  {
+    "revision": "ed3a84fc5308e9ff9a92faad7791be40",
+    "url": "/ihp/static/media/people1.ed3a84fc.jpeg"
+  },
+  {
+    "revision": "7b578ad53004deecadecf88daf2bed94",
+    "url": "/ihp/static/media/people10.7b578ad5.jpeg"
+  },
+  {
+    "revision": "3fee5320ba0fd50676813967ac1c1579",
+    "url": "/ihp/static/media/people11.3fee5320.jpeg"
+  },
+  {
+    "revision": "d70102163eeb63c79a38a193471a1053",
+    "url": "/ihp/static/media/people12.d7010216.jpeg"
+  },
+  {
+    "revision": "97853facde0457ec32d5aad4004026b8",
+    "url": "/ihp/static/media/people13.97853fac.jpeg"
+  },
+  {
+    "revision": "2ae405d73820acb05b8d5bc511a6f3d3",
+    "url": "/ihp/static/media/people14.2ae405d7.jpeg"
+  },
+  {
+    "revision": "72473ab847305d641b044e0b92a1d9cb",
+    "url": "/ihp/static/media/people15.72473ab8.jpeg"
+  },
+  {
+    "revision": "9e331351ffc97ba9e4856fe6c8ba6268",
+    "url": "/ihp/static/media/people2.9e331351.jpeg"
+  },
+  {
+    "revision": "02fc3c0123a9669e231678607159da90",
+    "url": "/ihp/static/media/people3.02fc3c01.jpeg"
+  },
+  {
+    "revision": "448d02944d56620b8e83b19fe99d2110",
+    "url": "/ihp/static/media/people4.448d0294.jpeg"
+  },
+  {
+    "revision": "8591ea7b9514784b77201f6865928830",
+    "url": "/ihp/static/media/people5.8591ea7b.jpeg"
+  },
+  {
+    "revision": "5d883de7d9c8167484b15d93a9802744",
+    "url": "/ihp/static/media/people6.5d883de7.jpeg"
+  },
+  {
+    "revision": "3baff0e1a56eac3ec57a37c20e962828",
+    "url": "/ihp/static/media/people7.3baff0e1.jpeg"
+  },
+  {
+    "revision": "81533c8c41d720f548d52e6237cf411a",
+    "url": "/ihp/static/media/people8.81533c8c.jpeg"
+  },
+  {
+    "revision": "882eb5b9dbddf831c74e2f51d3f3fb62",
+    "url": "/ihp/static/media/people9.882eb5b9.jpeg"
+  },
+  {
+    "revision": "fc33c0f0928e7e2d1d44499605508815",
+    "url": "/ihp/static/media/person-icon.fc33c0f0.svg"
+  },
+  {
+    "revision": "8648974fcf6b7d1436da65670916b277",
+    "url": "/ihp/static/media/product1.8648974f.jpeg"
+  },
+  {
+    "revision": "55e9620b7d5239660e3731bb1ccf2519",
+    "url": "/ihp/static/media/profile.55e9620b.jpg"
+  },
+  {
+    "revision": "1850eeb3bfa22fbc71f4b59d8e453215",
+    "url": "/ihp/static/media/question.1850eeb3.svg"
+  },
+  {
+    "revision": "1619579e618aa0ab5009496a94a30bcd",
+    "url": "/ihp/static/media/search-icon.1619579e.svg"
+  },
+  {
+    "revision": "5f91d8bf6a337320b398e3c0a28391c5",
+    "url": "/ihp/static/media/sidebar1.5f91d8bf.jpg"
+  },
+  {
+    "revision": "60a14ca5f70dafeccf498e88fef9c9b2",
+    "url": "/ihp/static/media/sidebar2.60a14ca5.jpg"
+  },
+  {
+    "revision": "1a56d2443a248f756fdcbb8ff009a65a",
+    "url": "/ihp/static/media/sidebar3.1a56d244.jpg"
+  },
+  {
+    "revision": "78491488cf664b1e9e69db7cbaa617ed",
+    "url": "/ihp/static/media/sidebar4.78491488.jpg"
+  },
+  {
+    "revision": "c0ab55699f7781fca01d8d8a842b7860",
+    "url": "/ihp/static/media/sidebar5.c0ab5569.jpg"
+  },
+  {
+    "revision": "9e40bab5361a2b7ae2ab62fa2410f90f",
+    "url": "/ihp/static/media/sidebar6.9e40bab5.jpg"
+  },
+  {
+    "revision": "f90a35dbefb06528076166c0b8bd12cb",
+    "url": "/ihp/static/media/sidebar7.f90a35db.jpg"
+  },
+  {
+    "revision": "6b3c204fa1329b9267e7c6523e021e6d",
+    "url": "/ihp/static/media/sidebar8.6b3c204f.jpg"
+  },
+  {
+    "revision": "f3f97260c445b16a68787684fb8b1b61",
+    "url": "/ihp/static/media/signin.f3f97260.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ihp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ihp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ihp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ihp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7687449ddb9f3d179da7dfd0a61bfa94",
+    "url": "/ihp/static/media/speaker.7687449d.jpeg"
+  },
+  {
+    "revision": "8ec0537727245c614454e07232ed38a9",
+    "url": "/ihp/static/media/success-email-icon.8ec05377.svg"
+  },
+  {
+    "revision": "b14e6ca50a41e4933c5db9557f71289d",
+    "url": "/ihp/static/media/top-diagnosis-bg.b14e6ca5.svg"
+  },
+  {
+    "revision": "84f55e14bd8a42616d2a34aa0e459a00",
+    "url": "/ihp/static/media/top-patient-bg.84f55e14.svg"
+  },
+  {
+    "revision": "3dd055c745e5cb0b37fe365e5ee2e2c4",
+    "url": "/ihp/static/media/total-panel-claim-value-bg.3dd055c7.svg"
+  },
+  {
+    "revision": "dd01d429797f5358edfa26493eee6c12",
+    "url": "/ihp/static/media/total-panel-claim-value-icon.dd01d429.svg"
+  },
+  {
+    "revision": "526f3adcc3d3d28b584e2ced11b1ef36",
+    "url": "/ihp/static/media/total-panel-claimants-bg.526f3adc.svg"
+  },
+  {
+    "revision": "de6fb694622fce23ff4ee17469671e46",
+    "url": "/ihp/static/media/total-panel-claimants-icon.de6fb694.svg"
+  },
+  {
+    "revision": "22547db55daade421417016def34a839",
+    "url": "/ihp/static/media/total-panel-claims-bg.22547db5.svg"
+  },
+  {
+    "revision": "e88810bbe38735b232da3949b5d37fa4",
+    "url": "/ihp/static/media/total-panel-claims-bg2.e88810bb.svg"
+  },
+  {
+    "revision": "3e1565852e8e9b6245e52ae0c8bc9807",
+    "url": "/ihp/static/media/total-panel-claims-icon.3e156585.svg"
+  },
+  {
+    "revision": "f19204b9d85dac3cc984e2d8db60bc11",
+    "url": "/ihp/static/media/toy.f19204b9.jpg"
+  },
+  {
+    "revision": "4f4446593ddbe22ae454841524446ec0",
+    "url": "/ihp/static/media/user.4f444659.svg"
+  },
+  {
+    "revision": "3d6f3748d306bee9e63359b3e4762d08",
+    "url": "/ihp/static/media/widget15-img.3d6f3748.png"
+  },
+  {
+    "revision": "9df0a6da12a039d3459d442c40ca6509",
+    "url": "/ihp/static/media/widget3-img2.9df0a6da.jfif"
+  },
+  {
+    "revision": "1fe0a07dff8496b9b8b0f616465c1abe",
+    "url": "/ihp/static/media/widget4-img.1fe0a07d.jpg"
+  },
+  {
+    "revision": "ff824c71604e87cb3d66e930afeed6bf",
+    "url": "/ihp/static/media/widget5-img3.ff824c71.jfif"
+  },
+  {
+    "revision": "1912dd232ac338722fe3e2bd19675c89",
+    "url": "/ihp/static/media/your-order.1912dd23.png"
+  },
+  {
+    "revision": "89ca21acc27983ed40596e14bb23be28",
+    "url": "/ihp/static/media/yourpayment.89ca21ac.png"
+  }
+]);
